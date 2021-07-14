6c2f8d98b9d1ab47e367dd82cf57c1e7fd0e1c62
unknown <chris.ramirez@revature.net>
2021-07-14 11:39:02 -0400

    Changed JRE to 1.8 and added first 3 labs

---
+++ Day3Labs/src/labs/DataTypes.java
@@ -0,0 +1,22 @@
+package labs;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		boolean on = false;
+		System.out.println(on);
+		short s = 16;
+		System.out.println(s);
+		int i = -32;
+		System.out.println(i);
+		float f = 3897.134F;
+		System.out.println(f);
+		long l = 12345678999L;
+		System.out.println(l);
+		char c = 'b';
+		System.out.println(c);
+		
+		
+	}
+
+}
---
+++ Day3Labs/src/labs/PrintName.java
@@ -0,0 +1,9 @@
+package labs;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		System.out.println("Chris Ramirez");
+	}
+
+}
