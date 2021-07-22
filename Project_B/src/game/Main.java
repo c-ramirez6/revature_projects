46672ae62d6ae3c8225fb48ae431c9b6be3024be
unknown <chris.ramirez@revature.net>
2021-07-22 14:45:51 -0400

    small changes

--- Project_B/src/game/Main.java
+++ Project_B/src/game/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import exceptions.FixtureDoesNotExistException;
 
 public class Main {
-	
+
 	private static final int NOUN = 1;
 	private static final int COMMAND = 0;
 	final static List<String> MOVE_COMMANDS = List.of("move", "go", "walk", "move to");
@@ -18,15 +18,14 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		String[] command = null;
 		Player player;
-		if(rm.roomMap.containsKey("n/a"))	{
+		if (rm.roomMap.containsKey("n/a")) {
 			player = new Player(rm.roomMap.get("n/a"));
-		}
-		else	{
+		} else {
 			player = new Player(rm.rooms.get(0));
 		}
 		System.out.println("Type help for list of commands as well as how to format commands");
 		System.out.println("Will perform better if you pay attention to case sensitivity");
-		do	{
+		do {
 			printRoom(player);
 			command = collectInput(sc);
 			try {
@@ -34,66 +33,60 @@ public class Main {
 			} catch (FixtureDoesNotExistException e) {
 				System.out.println(e.getMessage());
 			}
-		}while(command[COMMAND].compareToIgnoreCase("quit") != 0);
+		} while (command[COMMAND].compareToIgnoreCase("quit") != 0);
 		System.out.println("Thanks for playing!");
 	}
-	
+
 	private static void printRoom(Player player) {
 		System.out.println("Current room: " + player.currentRoom.name);
 		System.out.println(player.currentRoom.shortDescription);
 		System.out.println("You can see: \n");
-		for(String x : player.currentRoom.items.keySet()) {
+		for (String x : player.currentRoom.items.keySet()) {
 			System.out.println(x + ": " + player.currentRoom.items.get(x).shortDescription);
 		}
 	}
-	
-	private static String[] collectInput(Scanner sc)	{
-		return sc.nextLine().split(":");	
+
+	private static String[] collectInput(Scanner sc) {
+		return sc.nextLine().split(":");
 	}
-	
-	private static void parse(String[] command, Player player) throws FixtureDoesNotExistException	{
-		if(MOVE_COMMANDS.contains(command[COMMAND].toLowerCase()))	{
+
+	private static void parse(String[] command, Player player) throws FixtureDoesNotExistException {
+		if (MOVE_COMMANDS.contains(command[COMMAND].toLowerCase())) {
 			player.updateRoomOrder(player.currentRoom);
 			player.moveRooms(command[NOUN]);
-			if(player.currentRoom != null) {
+			if (player.currentRoom != null) {
 				System.out.println("You moved to the " + player.currentRoom.name + "\n");
-			}
-			else	{
+			} else {
 				player.currentRoom = player.getRoomOrder().pop();
 				throw new FixtureDoesNotExistException("Room does not exist");
 			}
-		}
-		else if(command[COMMAND].equalsIgnoreCase("help"))	{
+		} else if (command[COMMAND].equalsIgnoreCase("help")) {
 			displayHelp();
-		}
-		else if(OBSERVATION_COMMANDS.contains(command[COMMAND].toLowerCase()))	{
-			if(command.length == 1 || command[NOUN].equalsIgnoreCase("room") || command[NOUN].equalsIgnoreCase(player.currentRoom.name))	{
+		} else if (OBSERVATION_COMMANDS.contains(command[COMMAND].toLowerCase())) {
+			if (command.length == 1 || command[NOUN].equalsIgnoreCase("room")
+					|| command[NOUN].equalsIgnoreCase(player.currentRoom.name)) {
 				System.out.println("\n" + player.currentRoom.longDescription + "\n");
-			}
-			else if(player.currentRoom.items.get(command[NOUN]) != null)	{
-				System.out.println("\n" + player.currentRoom.items.get(command[NOUN]).name + ": " 
-			+ player.currentRoom.items.get(command[NOUN]).longDescription + "\n");
-			}
-			else	{
+			} else if (player.currentRoom.items.get(command[NOUN]) != null) {
+				System.out.println("\n" + player.currentRoom.items.get(command[NOUN]).name + ": "
+						+ player.currentRoom.items.get(command[NOUN]).longDescription + "\n");
+			} else {
 				throw new FixtureDoesNotExistException("Item does not exist");
 			}
-		}
-		else if(command[COMMAND].equalsIgnoreCase("back"))	{
-			try	{
+		} else if (command[COMMAND].equalsIgnoreCase("back")) {
+			try {
 				player.currentRoom = player.getRoomOrder().pop();
-			}	catch(java.util.EmptyStackException e) {
+			} catch (java.util.EmptyStackException e) {
 				System.out.println("You are at the first room.  Can not go back any farther");
 			}
-		}
-		else if(INTERACTION_COMMANDS.contains(command[COMMAND].toLowerCase()))	{
-			if(player.currentRoom.items.get(command[NOUN]) != null)	{
-				System.out.println("\n" + player.currentRoom.items.get(command[NOUN]).name + ": " 
-			+ player.currentRoom.items.get(command[NOUN]).getInteractDesc() + "\n");
+		} else if (INTERACTION_COMMANDS.contains(command[COMMAND].toLowerCase())) {
+			if (player.currentRoom.items.get(command[NOUN]) != null) {
+				System.out.println("\n" + player.currentRoom.items.get(command[NOUN]).name + ": "
+						+ player.currentRoom.items.get(command[NOUN]).getInteractDesc() + "\n");
 			}
 		}
 	}
-	
-	private static void displayHelp()	{
+
+	private static void displayHelp() {
 		System.out.println("Accepted command format is as follows");
 		System.out.println("<Command>:<Noun>");
 		System.out.println("When there is a target");
@@ -104,11 +97,11 @@ public class Main {
 		System.out.println("look:room");
 		System.out.println("Accepted Commands:");
 		System.out.println("Movement Commands:\n");
-		for(String x : MOVE_COMMANDS) {
+		for (String x : MOVE_COMMANDS) {
 			System.out.println(x);
 		}
 		System.out.println("\nObservation Commands:\n");
-		for(String x : OBSERVATION_COMMANDS) {
+		for (String x : OBSERVATION_COMMANDS) {
 			System.out.println(x);
 		}
 		System.out.println("\n");
--- Project_B/src/game/RoomManager.java
+++ Project_B/src/game/RoomManager.java
@@ -17,19 +17,19 @@ public class RoomManager {
 
 		try {
 			/*
-			 * Reads in layout folder and makes an array of files containing 
-			 * all of the files in the layout folder
+			 * Reads in layout folder and makes an array of files containing all of the
+			 * files in the layout folder
 			 */
 			File folder = new File("layout/");
 			File[] listOfFiles = folder.listFiles();
 			int i = 0;
-			if(listOfFiles != null && listOfFiles.length > 0) {
-				for(File file : listOfFiles) {
-					Scanner fileReader = new Scanner(file);	
+			if (listOfFiles != null && listOfFiles.length > 0) {
+				for (File file : listOfFiles) {
+					Scanner fileReader = new Scanner(file);
 					addRoom(fileReader, i);
 					while (fileReader.hasNextLine()) {
 						addItem(fileReader, i);
-				}
+					}
 					i++;
 					fileReader.close();
 				}
@@ -42,18 +42,16 @@ public class RoomManager {
 				for (Room x : rooms) {
 					printLoadIn(x);
 				}
-			}
-			else	{
+			} else {
 				nullLayout();
 			}
 
-		}catch (FileNotFoundException e) {
-		      System.out.println("Could not find layout folder or folder is empty");
-		      e.printStackTrace();
-		    }
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not find layout folder or folder is empty");
+			e.printStackTrace();
+		}
 	}
 
-	
 	/*
 	 * Prints the room and all of the connected rooms
 	 */
@@ -64,26 +62,27 @@ public class RoomManager {
 		}
 		System.out.println();
 	}
-	
+
 	/*
 	 * Adds room to room map and room array list
 	 */
-	private void addRoom(Scanner fileReader, int i)	{
+	private void addRoom(Scanner fileReader, int i) {
 		rooms.add(new Room(fileReader.nextLine(), fileReader.nextLine(), fileReader.nextLine(), fileReader.nextLine()));
 		roomMap.put(rooms.get(i).name, rooms.get(i));
 	}
-	
+
 	/*
 	 * Adds an item to rooms item map
 	 */
 	private void addItem(Scanner fileReader, int i) {
-		rooms.get(i).addItem(fileReader.nextLine(), fileReader.nextLine(), fileReader.nextLine(), fileReader.nextLine());
+		rooms.get(i).addItem(fileReader.nextLine(), fileReader.nextLine(), fileReader.nextLine(),
+				fileReader.nextLine());
 	}
-	
+
 	/*
 	 * Creates a room in case there is no files found
 	 */
-	private void nullLayout()	{
+	private void nullLayout() {
 		System.out.println("Could not find layout folder or folder is empty");
 		System.out.println("Generating default room");
 		rooms.add(new Room("n/a", "n/a", "n/a", "n/a"));
